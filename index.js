a208b15d3e95aaf6a998f2d81a1e49dabc2fcd30
dylanjenks <dylan.jenks92@gmail.com>
2021-09-14 03:18:41 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+    return x + y;
+  } 
+
+function subtract(x, y) {
+    return x - y;
+  } 
+
+function multiply(x, y) {
+    return x * y;
+  } 
+
+function divide(x, y) {
+    return x / y;
+  } 
+
+function increment(a) {
+    return ++a;
+  } 
+
+function decrement(a) {
+    return --a;
+  } 
+
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
